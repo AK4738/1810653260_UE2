a836d5a5f2001cab8925581ed10262837c055c96
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-22 14:06:51 +0100

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Geben Sie Ihr Name ein:"); //name als String eingeben
+        int alter = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie Ihr Alter ein")); //alter als integer eingeben und daher Integer.parseInt
+
+
+    }
+}
