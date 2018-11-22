aa676f70dcf5bda8e20e658c056a5e39b1e83de2
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-22 14:21:44 +0100

    volljährig

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         String name = JOptionPane.showInputDialog("Geben Sie Ihr Name ein:"); //name als String eingeben
         int alter = Integer.parseInt(JOptionPane.showInputDialog("Geben Sie Ihr Alter ein")); //alter als integer eingeben und daher Integer.parseInt
 
+        if(alter>17){
+            JOptionPane.showMessageDialog(null, "Sie sind volljährig"); // wenn er volljährig ist, ausgeben dass er volljähtig ist
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Sie sind nicht volljährig");
+        }
 
     }
 }
